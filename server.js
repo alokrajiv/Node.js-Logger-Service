c5f22c6cfc6b8fb94a80461134e29e7ef2321960
alokrajiv <mail@alokrajiv.com>
2016-07-14 12:22:26 +0530

    Added files

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    logger = require('morgan'),
+    fs = require('fs');
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+app.post('/data/', function (req, res) {
+    var filename = "test.txt";
+    var dataToSave = JSON.stringify(req.body);
+    console.log(dataToSave);
+    fs.unlink(filename, function (err) {
+        if (err && err.code !== 'ENOENT')
+            throw err;
+        fs.writeFile(filename, dataToSave, { flag: 'w' }, function (err) {
+            if (err) throw err;
+            res.json({ data: "saved" });
+        });
+    });
+});
+app.listen(process.env.PORT || 9001);
