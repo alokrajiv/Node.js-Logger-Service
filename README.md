833e9bddd8b2c3966ac5aeecae781ef4ea88c324
alokrajiv <mail@alokrajiv.com>
2016-07-14 12:32:15 +0530

    All Instructions and setting added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nodejs_save_kickstarter
+
+Simple Node.js Backend and jQuery HTML Web Interface to keep saving data to server in a text file.
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 app.post('/data/', function (req, res) {
